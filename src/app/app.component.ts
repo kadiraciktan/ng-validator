2c343e743f4f11f372b67460967daa2452e2c877
Oytun TUR <turoytun0@gmail.com>
2023-03-09 16:42:20 +0300

    feat:restyled the error message and added test form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,39 @@ import { Component } from '@angular/core';
 import { NgForm, Validators } from '@angular/forms';
 import { ExtendValidators } from './extendValidators';
 
+enum LoginType {
+  'Admin',
+  'User',
+}
+
+interface IForm {
+  userName: string;
+  email: string;
+  phone: string;
+  password: string;
+  loginType: LoginType | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  input: IForm = {
+    userName: '',
+    email: '',
+    phone: '',
+    password: '',
+    loginType: null,
+  };
+
+  loginTypes = LoginType;
+
   title = 'form-validator';
   userName = null;
   selectedValue = null;
+
   validators = ExtendValidators;
 
   onSubmit(ngForm: NgForm) {
